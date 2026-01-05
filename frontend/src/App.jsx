79f2b3809b67825a6cac4c4ca6fa8a7043c19850
aditibisht22 <aditi.bisht.2227@gmail.com>
2026-01-05 23:23:59 +0530

    set up tailwind and daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {Route,Routes} from "react-router";
+
+import Homepage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
+
 
 const App = () => {
   return (
-    <div>
-        
+    <div data-theme="valentine">
+    <button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-info">Info</button>
+<button className="btn btn-success">Success</button>
+<button className="btn btn-warning">Warning</button>
+<button className="btn btn-error">Error</button>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/create" element={<CreatePage/>} />
+          <Route path="/note/:id" element={<NoteDetailPage/>} />
+        </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteDetailPage = () => {
+  return (
+    <div>
+      blah
+    </div>
+  )
+}
+
+export default NoteDetailPage
