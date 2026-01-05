fd4143f52c4e774eea60d0b5caab5b082c354f33
aditibisht22 <aditi.bisht.2227@gmail.com>
2026-01-05 19:47:55 +0530

    update rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,23 +2,30 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import ratelimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 //middleware
-app.use(express.json());
+app.use(express.json()); //this middleware will parse JSON bodies: req.body
+
+//our simple custom middleware
+// app.use((req, res, next) =>{
+//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes", notesRoutes);
+app.use(ratelimiter);
 
-
-app.listen(5001, () =>{
+connectDB().then(()=>{
+    app.listen(5001, () =>{
     console.log("Server started on PORT:",PORT);
 });
 
+})
 
-//mongodb+srv://aditibisht2227_db_user:99P3JHKGDEPIWBtx@cluster0.pg1byiz.mongodb.net/?appName=Cluster0
+
