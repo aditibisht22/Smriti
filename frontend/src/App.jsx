e02b044c37bd845ba366076d9c258ceccedec00b
aditibisht22 <aditi.bisht.2227@gmail.com>
2026-01-06 00:39:33 +0530

    complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,14 +9,6 @@ import NoteDetailPage from "./pages/NoteDetailPage";
 const App = () => {
   return (
     <div data-theme="valentine">
-    <button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
         <Routes>
           <Route path="/" element={<Homepage/>} />
           <Route path="/create" element={<CreatePage/>} />
@@ -26,4 +18,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App;
